fafec97957349bb212bee71284111590101d870e
Koen Hufkens <khufkens@Pandora.local>
2016-10-29 19:04:45 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# equi2pano
+
+This script allows for easy ingestion of existing spherical panorama timelapse images into a [pano-moments input files](https://www.panomoments.com/). In particular, convert 120+ equirectangular files into pano-moment compatible files (half sized equirectangular files). The file names should not contain any spaces for the script to work.
+
+```bash
+# usage
+equi2pano.sh /your_equirectangular_files/ /your_output_dir/
+```
+
+Keep in mind that for static timelapse movies (fixed position) you will lose the parallax effect as advertised. Combining a 360 camera with the pano-moments rig will however preserve this quality.
+
+<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">VR needs a fun creative medium: 
+<a href="https://twitter.com/hashtag/PanoMoments?src=hash">#PanoMoments</a> - Easy to capture with motion parallax for more immersion - <a href="https://t.co/wbfyHCPDEl">https://t.co/wbfyHCPDEl</a><a href="https://t.co/iCuW4OkS74">pic.twitter.com/iCuW4OkS74</a></p>&mdash; Pano Moments (@PanoMoments) 
+<a href="https://twitter.com/PanoMoments/status/783300151750303744">October 4, 2016</a>
+</blockquote>
+
+You can alter the convert command in the script to adjust the field-of-view in order to use the 180 degree offset view instead. Unlike conventional cameras which only allow a singular view direction, 360 spherical cameras record the whole scene and allow for this creative freedom.
+
+More information and demo's can be found on the Pano Moments website:
+
+[https://www.panomoments.com/](https://www.panomoments.com/)
---
+++ equi2pano.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# This script allows for easy ingestion
+# of existing spherical panorama timelapse
+# images into a pano-moment input files.
+#
+# In particular, convert 120+ equirectangular files
+# into pano-moment compatible files
+# (half sized equirectangular files).
+# [Filenames should not contain spaces]
+
+# set input / output directory
+# [no checks at this time, will fail]
+input=$1
+output=$2
+
+# list all jpg files in an array
+# I assume that the directory only contains
+# equirectangular 360 spherical files which
+# are ordered chronologically (time series)
+files=($(ls $input/*.jpg))
+nrfiles=${#files[@]}
+
+# check if there are files to process
+if [ $nrfiles -le 1 ];
+then
+ echo "Too few files detected, check input directory!"
+ exit 1
+fi
+
+# grab the X resolution of the image and
+# determine the offset per image step
+# (rounded up)
+xres=`identify ${files[1]} | cut -d' ' -f3 | cut -d'x' -f1`
+offset=`echo "$xres / ( $nrfiles - 1 )" | bc -l | awk '{printf("%d\n",$1 + 0.5)} ' `
+
+# loop over the first 120 files
+# regardless of the number of files
+# in the directory
+f=1
+for i in `seq 0 ${offset} ${xres}`;
+do	
+
+	# Roll the image while stepping through the files!
+	
+	# You can change the starting point (or vantage point)
+	# by adding another roll offset before the sequential one:
+	# e.g. -roll +2600x0 -roll -${i}x0
+	#
+	# In fast moving acquisitions in single lens setups you are limited
+	# to one field-of-view. This is does not apply to spherical cameras
+	# which allows you to pick the opposite viewpoint.
+	convert ${files[$f]} -roll -${i}x0 -crop 50%x100%+0+0 $output/panomoment_${f}.jpg
+
+	# increase counter
+	f=`echo $f + 1 | bc`
+	
+done
+
+exit 1
